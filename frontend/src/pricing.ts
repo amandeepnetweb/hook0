0e1f9a092121e4ac3f76acd218651108ddbb8e87
ThomasTartrau <thomas.tartrau@ecoles-epsi.net>
2025-02-11 14:50:09 +0100

    feat(frontend): add support email adress to configuration field

--- frontend/src/pricing.ts
+++
@@ -1,6 +0,0 @@
-import { getInstanceConfig } from './utils/biscuit_auth';
-
-export async function isPricingEnabled(): Promise<boolean> {
-  const config = await getInstanceConfig();
-  return config.quota_enforcement;
-}
